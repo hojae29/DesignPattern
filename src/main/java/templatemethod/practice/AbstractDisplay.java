c2de160b61fc017867ccf64e9f65d8f8fe8b1dc3
hojae29 <gkqthd6386@naver.com>
2023-02-12 23:12:23 +0900

    Template Method 패턴

---
+++ src/main/java/templatemethod/practice/AbstractDisplay.java
@@ -0,0 +1,17 @@
+package templatemethod.practice;
+
+public interface AbstractDisplay {
+    public void open();
+
+    public void print();
+
+    public void close();
+
+    public default void display() {
+        open();
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+        close();
+    }
+}
