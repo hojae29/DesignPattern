637b24a25808138564602414fc0d9887e19fe254
hojae29 <gkqthd6386@naver.com>
2023-02-19 21:26:35 +0900

    Singleton 패턴

---
+++ src/main/java/singleton/practice/TicketMaker.java
@@ -0,0 +1,16 @@
+package singleton.practice;
+
+public class TicketMaker {
+    private static TicketMaker ticketMaker = new TicketMaker();
+    private int ticket = 1000;
+
+    private TicketMaker() {}
+
+    public synchronized int getNextTicketNumber() {
+        return ticket++;
+    }
+
+    public static TicketMaker getInstance() {
+        return ticketMaker;
+    }
+}
---
+++ src/main/java/singleton/practice/TicketMakerMain.java
@@ -0,0 +1,12 @@
+package singleton.practice;
+
+public class TicketMakerMain {
+
+    public static void main(String[] args) {
+        System.out.println("Start.");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ":" + TicketMaker.getInstance().getNextTicketNumber());
+        }
+        System.out.println("End.");
+    }
+}
